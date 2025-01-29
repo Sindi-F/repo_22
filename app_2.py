002cd62700167aeedd281ae401ef949825833806
Sindi-F <103183308+Sindi-F@users.noreply.github.com>
2025-01-29 14:24:29 +0200

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -14,4 +14,4 @@ st.write("Hello, Streamlit!")
 st.header("Number selection")
 
 number = st.slider("Pick a number", 1, 100)
-st.write(f"You picked: {number}")
+st.write(f"You have picked: {number}")
